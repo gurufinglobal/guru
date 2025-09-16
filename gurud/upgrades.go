fb063761d2bf262384d5d0952869535c40b6c7a9
Eddy <comnic@gurufin.com>
2025-09-16 15:02:21 +0900

    feat: cosmovisor test

--- gurud/upgrades.go
+++ gurud/upgrades.go
@@ -1,5 +1,20 @@
 package gurud
 
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
 func (app EVMD) RegisterUpgradeHandlers() {
-	// No upgrades registered yet
+	// v201-to-v202 upgrade handler
+	app.UpgradeKeeper.SetUpgradeHandler(
+		"v201-to-v202",
+		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			// Upgrade logic here
+			// For a simple upgrade without state migrations, just return the current version map
+			return app.ModuleManager.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
 }
