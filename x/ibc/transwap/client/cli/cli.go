6154419e01fcc9285b0be599cea5c35c6729d029
Tillo Tulkinbekov <tulkinbekov@gurufin.com>
2025-11-11 13:08:48 +0900

    feat: init transwap module

---
+++ x/ibc/transwap/client/cli/cli.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// GetQueryCmd returns the query commands for IBC connections
+func GetQueryCmd() *cobra.Command {
+	queryCmd := &cobra.Command{
+		Use:                        "ibc-swapt",
+		Short:                      "IBC fungible token transfer query subcommands",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+	}
+
+	queryCmd.AddCommand(
+		GetCmdQueryDenom(),
+		GetCmdQueryDenoms(),
+		GetCmdQueryEscrowAddress(),
+		GetCmdQueryDenomHash(),
+		GetCmdQueryTotalEscrowForDenom(),
+	)
+
+	return queryCmd
+}
